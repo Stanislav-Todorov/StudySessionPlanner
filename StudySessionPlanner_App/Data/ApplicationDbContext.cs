d79a92ab234fdd048025fe67bcfd9c939e67b927
Stanislav Todorov <stanislav.r.todorov@gmail.com>
2026-02-08 15:00:06 +0200

    Add project files.

---
+++ StudySessionPlanner_App/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudySessionPlanner_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ StudySessionPlanner_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudySessionPlanner_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
