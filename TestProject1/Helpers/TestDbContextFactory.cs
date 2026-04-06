b4ff9a3fb8ec78eae8ff0409905511d68efa9504
Stanislav Todorov <stanislav.r.todorov@gmail.com>
2026-04-06 18:21:55 +0300

    Added unit test project and test database setup.

---
+++ TestProject1/Helpers/TestDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudySessionPlanner_App.Data;
+
+namespace StudySessionPlanner.Tests.Helpers
+{
+    public static class TestDbContextFactory
+    {
+        public static ApplicationDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace StudySessionPlanner_App.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
